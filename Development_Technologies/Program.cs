f3b5edc63d3ea13cc974072c5d6a1c5eee26008b
Екатерина <katevoronina128@gmail.com>
2021-10-12 21:38:41 +0400

    Добавьте файлы проекта.

---
+++ Development_Technologies/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+namespace Development_Technologies
+{
+    public class Name
+    {
+        public string name;
+        public string surname;
+        public string thirdname;
+
+        public Name(string s, string n, string sec)
+        {
+            name = n;
+            surname = s;
+            thirdname = sec;
+
+        }
+
+        public void Show()
+        {
+            Console.WriteLine("{0} {1} {2}", surname, name, thirdname);
+        }
+
+    }
+
+    public class Program
+    {
+
+        static void Main(string[] args)
+        {
+            string end = @"\data\FirstEx.txt";   
+            string path = Directory.GetParent(Directory.GetCurrentDirectory()).Parent.FullName;
+            List<Name> spisok = new List<Name>();
+
+            using (StreamReader input = new StreamReader(path + end))
+            {
+           
+                while (!input.EndOfStream)
+                {
+                    string[] tmp = input.ReadLine().Split(' ');
+                    Name person = new Name(tmp[0], tmp[1], tmp[2]);
+                    spisok.Add(person);
+
+                }
+
+            }
+
+
+            var sorted = spisok.OrderBy(t => t.surname)
+                .ThenByDescending(t => t.name).ThenByDescending(t => t.surname).ThenByDescending(t => t.thirdname);
+
+            foreach (var elem in sorted)
+            {
+                elem.Show();
+
+            }
+
+
+
+
+        }
+    }
+}
+
+
---
+++ Spusk/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+namespace Spusk
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string end = @"\data\SecEx.txt";
+            string path = Directory.GetParent(Directory.GetCurrentDirectory()).Parent.FullName;
+            using (StreamReader reader = new StreamReader(path + end))
+            {
+                int n = int.Parse(reader.ReadLine());
+                int index = 0;
+
+                string tm = reader.ReadToEnd();
+                StringBuilder a = new StringBuilder(tm);
+                for (int i = 0; i < a.Length;)
+                {
+                    if (char.IsPunctuation(a[i]))
+                    {
+                        a.Remove(i, 1);
+                    }
+                    else
+                    {
+                        if (char.IsWhiteSpace(a[i]))
+                        {
+                            a[i] = ' ';
+                        }
+                        ++i;
+                    }
+                }
+                string[] tmp = a.ToString().Trim().Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+
+                int[][] myArr = new int[n][];
+                for (int i = 0; i < n; i++)
+                {
+                    myArr[i] = new int[i];
+                }
+                int[,] mas = new int[n, n];
+
+                for (int i = 0; i < n; i++)
+                {
+                    for (int j = 0; j < i + 1; j++)
+                    {
+                        mas[i, j] = int.Parse(tmp[index]);
+                        index++;
+                    }
+
+                }
+
+                for (int i = n - 1; i > 0; i--)
+                {
+                    for (int j = 0; j < n - 1; j++)
+                    {
+                        int max = (mas[i, j] > mas[i, j + 1]) ? mas[i, j] : mas[i, j + 1];
+                        mas[i - 1, j] += max;
+                    }
+
+                }
+
+                Console.WriteLine(mas[0, 0]);
+
+            }
+
+        }
+    }
+}
