ba1a5dd13463f1598b3d76d81dd6a320acf8372f
Дмитрий Сазонов <sqzonow@outlook.com>
2021-10-26 20:41:23 +0400

    In progress...

--- CustomDictionary/CustomDic.cs
+++ CustomDictionary/CustomDic.cs
@@ -10,68 +10,87 @@ namespace CustomDictionary
     public class CustomDic<TKey, TValue> : ICollection<KeyValuePair<TKey, TValue>>, IEnumerable<KeyValuePair<TKey, TValue>>, IEnumerable
         {
             private List<TValue>[] _items;
+  
+        public int Count() => _items.Count();
 
-            public CustomDic(int size)
+        int ICollection<KeyValuePair<TKey, TValue>>.Count => throw new NotImplementedException();
+        public bool IsReadOnly => throw new NotImplementedException();
+
+        public CustomDic(int size)
             {
                 _items = new List<TValue>[size];
             }
 
-            private int GetHash(TKey item)
+            private int GetHash(TKey key)
             {
-                return item.ToString().Length % _items.Length;
+                return Convert.ToInt32(key.ToString().Substring(0,1));
             }
 
             public void Add(TKey key, TValue value)
             {
                 var k = GetHash(key);
-                _items[k].Add(value);
+            if (_items[k] == null)
+            {
+                _items[k] = new List<TValue>() { value };//*
+            }
+            else
+            {
+                _items[k].Add(value);//*
+            }
             }
             public bool Search(TKey key, TValue value)
             {
                 var k = GetHash(key);
-                return _items[k].Equals(value);
+                return _items[k].Contains(value);//*
             }
-
-            public int Count => throw new NotImplementedException();
-
-            public bool IsReadOnly => throw new NotImplementedException();
-
-            public void Add(KeyValuePair<TKey, TValue> item)
+       public  void Remove(TKey key, TValue value)
+        {
+            var k = GetHash(key);
+            if (_items[k] == null)  
+            {}
+            else
             {
-                throw new NotImplementedException();
+                _items[k].Remove(value);//*
             }
+        }
+   
 
-            public void Clear()
-            {
-                throw new NotImplementedException();
-            }
+        void ICollection<KeyValuePair<TKey, TValue>>.Add(KeyValuePair<TKey, TValue> item)
+        {
+            throw new NotImplementedException();
+        }
 
-            public bool Contains(KeyValuePair<TKey, TValue> item)
-            {
-                throw new NotImplementedException();
-            }
+        void ICollection<KeyValuePair<TKey, TValue>>.Clear()
+        {
+            throw new NotImplementedException();
+        }
 
-            public void CopyTo(KeyValuePair<TKey, TValue>[] array, int arrayIndex)
-            {
-                throw new NotImplementedException();
-            }
+        bool ICollection<KeyValuePair<TKey, TValue>>.Contains(KeyValuePair<TKey, TValue> item)
+        {
+            throw new NotImplementedException();
+        }
 
-            public IEnumerator<KeyValuePair<TKey, TValue>> GetEnumerator()
-            {
-                throw new NotImplementedException();
-            }
-
-            public bool Remove(KeyValuePair<TKey, TValue> item)
-            {
-                throw new NotImplementedException();
-            }
-
-            IEnumerator IEnumerable.GetEnumerator()
-            {
-                throw new NotImplementedException();
-            }
+        void ICollection<KeyValuePair<TKey, TValue>>.CopyTo(KeyValuePair<TKey, TValue>[] array, int arrayIndex)
+        {
+            throw new NotImplementedException();
+        }
 
        
+
+        IEnumerator<KeyValuePair<TKey, TValue>> IEnumerable<KeyValuePair<TKey, TValue>>.GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Remove(KeyValuePair<TKey, TValue> item)
+        {
+            throw new NotImplementedException();
+        }
     }
     }
 
---
+++ CustomDictionary/Programm.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomDictionary
+{
+    class Programm
+    {
+
+        static void Main(string[] args)
+        {
+            var hashtb = new CustomDic<int, string>(100);
+            hashtb.Add(1, "Hash");
+            hashtb.Add(11, "SGU");
+            hashtb.Add(45, "EPAM");
+
+            Console.WriteLine(hashtb.Search(45, "EPAM"));
+
+        }
+
+
+    }
+}
