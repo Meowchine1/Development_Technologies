9f1310051346cc33f084817fd1e9a22deab09b22
Дмитрий Сазонов <sqzonow@outlook.com>
2021-10-26 12:24:04 +0400

    Add CustomDictionary class

---
+++ CustomDictionary/CustomDic.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomDictionary
+{
+    public class CustomDic<TKey, TValue> : ICollection<KeyValuePair<TKey, TValue>>, IEnumerable<KeyValuePair<TKey, TValue>>, IEnumerable
+        {
+            private List<TValue>[] _items;
+
+            public CustomDic(int size)
+            {
+                _items = new List<TValue>[size];
+            }
+
+            private int GetHash(TKey item)
+            {
+                return item.ToString().Length % _items.Length;
+            }
+
+            public void Add(TKey key, TValue value)
+            {
+                var k = GetHash(key);
+                _items[k].Add(value);
+            }
+            public bool Search(TKey key, TValue value)
+            {
+                var k = GetHash(key);
+                return _items[k].Equals(value);
+            }
+
+            public int Count => throw new NotImplementedException();
+
+            public bool IsReadOnly => throw new NotImplementedException();
+
+            public void Add(KeyValuePair<TKey, TValue> item)
+            {
+                throw new NotImplementedException();
+            }
+
+            public void Clear()
+            {
+                throw new NotImplementedException();
+            }
+
+            public bool Contains(KeyValuePair<TKey, TValue> item)
+            {
+                throw new NotImplementedException();
+            }
+
+            public void CopyTo(KeyValuePair<TKey, TValue>[] array, int arrayIndex)
+            {
+                throw new NotImplementedException();
+            }
+
+            public IEnumerator<KeyValuePair<TKey, TValue>> GetEnumerator()
+            {
+                throw new NotImplementedException();
+            }
+
+            public bool Remove(KeyValuePair<TKey, TValue> item)
+            {
+                throw new NotImplementedException();
+            }
+
+            IEnumerator IEnumerable.GetEnumerator()
+            {
+                throw new NotImplementedException();
+            }
+
+       
+    }
+    }
+
